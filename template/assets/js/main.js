3c5bdf62fc72ab9be168b149b0b546d62254b084
sajmon91 <stefan.sajmon@gmail.com>
2022-06-27 15:23:39 +0200

    finished home page

--- template/assets/js/main.js
+++ template/assets/js/main.js
@@ -14,3 +14,15 @@ const sidebar = document.querySelector(".sidebar");
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("open");
 });
+
+// videos hover
+const allVideos = document.querySelectorAll(".videoPlay");
+
+allVideos.forEach((video) => {
+  video.addEventListener("mouseover", () => {
+    video.play();
+  });
+  video.addEventListener("mouseleave", () => {
+    video.pause();
+  });
+});
