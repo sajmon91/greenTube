46f59692edc6f0e2053f8cfa145c326609068e3b
sajmon91 <stefan.sajmon@gmail.com>
2022-06-27 13:57:07 +0200

    finished sidebar and mobnav

--- template/assets/js/main.js
+++ template/assets/js/main.js
@@ -1,4 +1,16 @@
 //  menu dropdown
-document.querySelector(".dropBtn").addEventListener("click", () => {
-  document.querySelector(".dropdownContent").classList.toggle("show");
+const menuDropdown = document.querySelector(".dropBtn");
+
+if (menuDropdown) {
+  menuDropdown.addEventListener("click", () => {
+    document.querySelector(".dropdownContent").classList.toggle("show");
+  });
+}
+
+// sidebar
+const menuBtn = document.querySelector(".menuIcon");
+const sidebar = document.querySelector(".sidebar");
+
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
 });
