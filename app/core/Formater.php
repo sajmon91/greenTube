eb1898b4b7c8bb23135be707e5dc9da1c440f234
sajmon91 <stefan.sajmon@gmail.com>
2026-01-17 11:16:12 +0100

    Fix PHP 8.2 deprecated warning in Formater::timeAgo

--- app/core/Formater.php
+++ app/core/Formater.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class Formater
 {
@@ -6,7 +6,8 @@ class Formater
 	/**
 	 * Formatting number 1420 -> 1.4K
 	 */
-	public static function getFormattedNumber($value, $locale = 'en_US'){
+	public static function getFormattedNumber($value, $locale = 'en_US')
+	{
 		$style = NumberFormatter::PADDING_POSITION;
 		$formatter = new NumberFormatter($locale, $style);
 
@@ -18,32 +19,55 @@ class Formater
 	/**
 	 * Time ago formatting
 	 */
-	public static function timeAgo($datetime, $full = false) {
-		$now = new DateTime;
+	public static function timeAgo($datetime, $full = false)
+	{
+		$now = new DateTime();
 		$ago = new DateTime($datetime);
 		$diff = $now->diff($ago);
 
-		$diff->w = floor($diff->d / 7);
-		$diff->d -= $diff->w * 7;
+		$weeks = floor($diff->d / 7);
+		$days  = $diff->d - ($weeks * 7);
 
 		$string = [
-		    'y' => 'year',
-		    'm' => 'month',
-		    'w' => 'week',
-		    'd' => 'day',
-		    'h' => 'hour',
-		    'i' => 'minute',
-		    's' => 'second',
+			'y' => 'year',
+			'm' => 'month',
+			'w' => 'week',
+			'd' => 'day',
+			'h' => 'hour',
+			'i' => 'minute',
+			's' => 'second',
 		];
+
 		foreach ($string as $k => &$v) {
-		    if ($diff->$k) {
-		        $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
-		    } else {
-		        unset($string[$k]);
-		    }
+			switch ($k) {
+				case 'w':
+					if ($weeks) {
+						$v = $weeks . ' ' . $v . ($weeks > 1 ? 's' : '');
+					} else {
+						unset($string[$k]);
+					}
+					break;
+
+				case 'd':
+					if ($days) {
+						$v = $days . ' ' . $v . ($days > 1 ? 's' : '');
+					} else {
+						unset($string[$k]);
+					}
+					break;
+
+				default:
+					if ($diff->$k) {
+						$v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+					} else {
+						unset($string[$k]);
+					}
+			}
 		}
 
-		if (!$full) $string = array_slice($string, 0, 1);
+		if (!$full) {
+			$string = array_slice($string, 0, 1);
+		}
 
 		return $string ? implode(', ', $string) . ' ago' : 'just now';
 	}
@@ -68,7 +92,7 @@ class Formater
 		return date("j M Y", strtotime($date));
 	}
 
-/////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -76,7 +100,3 @@ class Formater
 
 
 } // end class
-
-
-
-?>
